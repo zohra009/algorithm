7c77342e7ba1734e3c9a4a7dbeec54ddecce06d1
zor009 <anzohra@outlook.com>
2019-12-16 22:42:31 -0500

    maxchar

--- session1/cap-first-letter.js
+++ session1/cap-first-letter.js
@@ -28,30 +28,32 @@
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
+// CHALLENGE 5: MAX CHARACTER
+// Return the character that is most common in a string
+// ex. maxCharacter('javascript') == 'a'
 function maxCharacter(str) {
-    //initialize char
+    //initialize charMap
     const charMap = {};
     let maxNum = 0;
     let maxChar = '';
-
-    str.split('').forEach(char => {
-        if(charMap[char]) {
-            charMap[char]++
-        }else {
-            charMap[char] = 1; 
-        }
-        
-    })
-    }
-    for(let char in charMap){
-        if(charMap[char] > maxNum){
-        maxNum = charMap[char]
+  
+    str.split('').forEach(function(char) {
+      if(charMap[char]) {
+        charMap[char]++;
+      } else {
+        charMap[char] = 1;
+      }
+    });
+  
+    for(let char in charMap) {
+      // debugger;
+      if(charMap[char] > maxNum) {
+        maxNum = charMap[char];
         maxChar = char;
-     }
+      }
     }
-   return maxChar
-    
-}
+  
+    return maxChar;
 
 
 maxCharacter("I love new yearssssssss")
